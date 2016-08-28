91a83a3b5d7cc1ef5115b37a5b0e88569ec228df
Markus Kobler <markus.kobler@digital.hmrc.gov.uk>
2016-08-28 17:36:55 +0100

    Initial commit

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# Download and build libslz
+set -euo pipefail
+
+declare -r version=1.0.0
+declare -r sha256=4696da81bc4ac5ec5bbc36a62c168bdca4d35f4fbe63da76b2d9cf54be857a45
+
+declare -r build=${PWD}/libslz
+declare -r out=${PWD}/libslz-build
+
+mkdir -p ${build}
+
+curl -OL https://github.com/haproxy/libslz/archive/v${version}.tar.gz
+echo "${sha256}  v${version}.tar.gz" | sha256sum -c
+tar -C ${build} --strip-components 1 -xf v${version}.tar.gz
+
+cd ${build}
+make
+make install PREFIX=${out}
