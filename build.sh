ad2c5503aa084ab7fefd3cceb7ceb9272e7973b5
Markus Kobler <markus.kobler@digital.hmrc.gov.uk>
2016-08-28 19:12:29 +0100

    Bringing naming inline

--- build.sh
+++ build.sh
@@ -6,15 +6,14 @@ set -euo pipefail
 version=1.0.0
 sha256=4696da81bc4ac5ec5bbc36a62c168bdca4d35f4fbe63da76b2d9cf54be857a45
 
-build=${PWD}/libslz
+src=${PWD}/libslz
 out=${PWD}/libslz-build
 
-mkdir -p ${build}
-
+mkdir -p ${src}
 curl -OL https://github.com/haproxy/libslz/archive/v${version}.tar.gz
 echo "${sha256}  v${version}.tar.gz" | sha256sum -c
-tar -C ${build} --strip-components 1 -xf v${version}.tar.gz
+tar -C ${src} --strip-components 1 -xf v${version}.tar.gz
 
-cd ${build}
+cd ${src}
 make
 make install PREFIX=${out}
